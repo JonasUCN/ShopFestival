badca7d6a17115704ae23f46024bdda55ad75997
Christopher <93257325+CsnUcn@users.noreply.github.com>
2022-11-08 15:13:04 +0100

    added tests

--- WebShop/WebShop/Controllers/HomeController.cs
+++ WebShop/WebShop/Controllers/HomeController.cs
@@ -12,7 +12,6 @@ namespace WebShop.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
-        private CartController _CartController = new();
 
         public HomeController(ILogger<HomeController> logger)
         {
@@ -27,31 +26,6 @@ namespace WebShop.Controllers
         public IActionResult Privacy()
         {
             return View();
-        }
-
-        public IActionResult ProductView() //TODO Make it to take a para as int id to custom take what product to display
-        {
-
-            string url = "https://localhost:5001/api/Product/Products/3";
-            var client = new RestClient(url);
-            var response = client.Get(new RestRequest());
-            Product product = JsonConvert.DeserializeObject<Product>(response.Content);
-
-            return View(product);
-        }
-
-        [HttpPost]
-        public IActionResult ProductView(Product _Product)
-        {
-            string url = "https://localhost:5001/api/Product/RemoveStock/" + _Product.id;
-            var client = new RestClient(url);
-            var response = client.Post(new RestRequest());
-
-            if (response.StatusCode == System.Net.HttpStatusCode.OK)
-            {
-                _CartController.addProductToCart(_Product);
-            }
-            return View(_Product);
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
---
+++ WebShop/WebShop/Controllers/ProductController.cs
@@ -0,0 +1,48 @@
+﻿using LayerController;
+using Microsoft.AspNetCore.Mvc;
+using ModelLayer;
+using Newtonsoft.Json;
+using RestSharp;
+
+namespace WebShop.Controllers
+{
+    public class ProductController : Controller
+    {
+        private CartController _CartController = new();
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult ProductView(int id) //TODO Make it to take a para as int id to custom take what product to display
+        {
+
+            string url = "https://localhost:5001/api/Product/Products/" + id;
+            var client = new RestClient(url);
+            var response = client.Get(new RestRequest());
+            Product product = JsonConvert.DeserializeObject<Product>(response.Content);
+
+            return View(product);
+        }
+
+        [HttpPost]
+        public IActionResult ProductView(Product _Product)
+        {
+            string url = "https://localhost:5001/api/Product/RemoveStock/" + _Product.id;
+            var client = new RestClient(url);
+            var response = client.Post(new RestRequest());
+
+            if (response.StatusCode == System.Net.HttpStatusCode.OK)
+            {
+                _CartController.addProductToCart(_Product);
+            }
+            return View(_Product);
+        }
+
+        public CartController GetCartController()
+        {
+            return _CartController;
+        }
+    }
+}
