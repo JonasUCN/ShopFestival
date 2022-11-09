efb6bc3bcc7594ead603e7574a61300f5cd471c5
Jesper Johannsen <70481328+Jesperejohannsen@users.noreply.github.com>
2022-11-09 12:55:27 +0100

    Lav en produktoversigt1
    Synkronisere databsen med et bootstrap view.

--- WebShop/WebShop/Controllers/HomeController.cs
+++ WebShop/WebShop/Controllers/HomeController.cs
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 ﻿using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 using WebShop.Models;
@@ -29,4 +30,42 @@ namespace WebShop.Controllers
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
+=======
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using WebShop.Models;
+using ModelLayer;
+using Database_Service.LogicController;
+using Newtonsoft.Json;
+using RestSharp;
+using LayerController;
+
+namespace WebShop.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Produkter()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+>>>>>>> Stashed changes
 }
--- WebShop/WebShop/Controllers/ProductController.cs
+++ WebShop/WebShop/Controllers/ProductController.cs
@@ -7,10 +7,29 @@ namespace WebShop.Controllers
 {
     public class ProductController : Controller
     {
+<<<<<<< Updated upstream
         LogicProductController _ProductController;
         public ProductController(LogicProductController productController, List<Product> products)
         {
             _ProductController = new LogicProductController();
+=======
+        private CartController _CartController = new();
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult ProductView() //TODO Make it to take a para as int id to custom take what product to display
+        {
+
+            string url = "https://localhost:5001/api/Product/Products/3";
+            var client = new RestClient(url);
+            var response = client.Get(new RestRequest());
+            Product product = JsonConvert.DeserializeObject<Product>(response.Content);
+
+            return View(product);
+>>>>>>> Stashed changes
         }
 
         //private readonly List<Product> products = _ProductController.GetAllProducts();
