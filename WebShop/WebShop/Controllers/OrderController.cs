8eb7d7163d1f4dc32fe6712ee2bd297d6df6489b
10407499 <10407499@ucn.dk>
2022-11-14 13:36:57 +0100

    IkkeFærdig.Task

--- WebShop/WebShop/Controllers/OrderController.cs
+++ WebShop/WebShop/Controllers/OrderController.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using ModelLayer.DTO;
+using ModelLayer;
 
 namespace WebShop.Controllers
 {
@@ -6,6 +8,19 @@ namespace WebShop.Controllers
     {
         
         public IActionResult OrderView()
+        {
+            ModelOrderView mov = new ModelOrderView();
+            Product product = new Product() { Brand = "Addias", Price = 10, ProductDesc = "Festivals Sko", Stock = 50, Title = "Festivals sko", id = 3 };
+            OrderLine orderLine = new OrderLine { Product = product, Quantity = 21 };
+            Cart cart = new Cart();
+            cart.addOrderLine(orderLine);
+            mov.cart = cart;
+
+            return View(mov);
+        }
+
+        [HttpPost]
+        public IActionResult OrderView(ModelOrderView _MOV)
         {
             
 
